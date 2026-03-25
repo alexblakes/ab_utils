e638ea1e4bff6317fa9021057b8b03b27a414edb
Alex <alex.blakes@gmail.com>
2026-03-25 15:52:43 +0000

    Inject snakemake

--- ab_utils/__init__.py
+++ ab_utils/__init__.py
@@ -3,4 +3,4 @@ import matplotlib.pyplot as plt
 from . import logging
 from . import vis
 from .method_chains import flatten_columns, assign_with_apply
-from .smk_utils import mock_snakemake
+from .smk_utils import inject_snakemake
