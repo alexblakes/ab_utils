b3b82ef889d812da4221836c34cedc3eebdf8a1a
Alex <alex.blakes@gmail.com>
2026-03-25 15:50:37 +0000

    Add smk utils

--- ab_utils/__init__.py
+++ ab_utils/__init__.py
@@ -3,3 +3,4 @@ import matplotlib.pyplot as plt
 from . import logging
 from . import vis
 from .method_chains import flatten_columns, assign_with_apply
+from .smk_utils import mock_snakemake
--- ab_utils/vis/__init__.py
+++ ab_utils/vis/__init__.py
@@ -10,7 +10,4 @@ from .color import adjust_alpha
 RED = cm.Reds(0.8)
 BLUE = cm.Blues(0.8)
 
-# Styles - load the default mplstyle by file path
 plt.style.use("ab_utils.vis.style.default")
-# style_file = files("ab_utils.vis.style").joinpath("default.mplstyle")
-# plt.style.use(str(style_file))
