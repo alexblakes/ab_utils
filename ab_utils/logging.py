223d4bfaad9063e79c1286124fa4b515601c5de4
Alex <alex.blakes@gmail.com>
2026-03-27 11:49:43 +0000

    Update pandas checks logging

--- ab_utils/logging.py
+++ ab_utils/logging.py
@@ -1,7 +1,14 @@
 import inspect
 import logging
+from pathlib import Path
+from turtle import pd
 
 import pandas_checks as pdc
+
+FORMAT = "[{asci_time}] [{levelname}] || {message}"
+
+logging.basicConfig(format=FORMAT, style="{", level=logging.INFO)
+logger = logging.getLogger()
 
 
 def get_calling_fn(*ignore: str) -> str | None:
@@ -15,7 +22,6 @@ def get_calling_fn(*ignore: str) -> str | None:
     ignore_set = set(ignore) | default_ignore
 
     for frameinfo in inspect.stack()[1:]:
-
         # skip functions in this module
         module_name = frameinfo.frame.f_globals.get("__name__", "")
         if module_name == __name__:
@@ -29,9 +35,27 @@ def get_calling_fn(*ignore: str) -> str | None:
     return None
 
 
-# Logging
-logger = logging.getLogger(__name__)
+# Set up logging via Snakemake if possible
+try:
+    from snakemake.script import snakemake
+
+    file_log = Path(snakemake.log[0]).resolve()
+except (ImportError, IndexError):
+    logger.info("No Snakemake log file detected. Logging to stderr only.")
+else:
+    file_handler = logging.FileHandler(file_log, mode="w")
+    file_handler.setFormatter(logging.Formatter(FORMAT, style="{"))
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    logger.info(f"Logging to stderr and '{file_log}' via Snakemake")
+
 
 # Pandas checks
-pdc.set_custom_print_fn(logger.info, print_to_stdout=False)
-pdc.set_format(precision=3, use_emojis=False)
+pdc.set_format(precision=4, use_emojis=False)
+
+if not pd.core.config_init.is_terminal():
+    # Use default print function in interactive envs
+    pdc.set_custom_print_fn(None, print_to_stdout=True)
+else:
+    # Use logger in terminal
+    pdc.set_custom_print_fn(logger.info, print_to_stdout=False)
