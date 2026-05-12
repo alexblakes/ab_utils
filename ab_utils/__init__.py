8d52a0f13040a55256f520d69482e45a2cd48ea6
Alex <alex.blakes@gmail.com>
2026-05-12 15:49:31 +0100

    Revert "Add global method"
    This reverts commit 9672e9e2d13c302a9d663d6f720d97cc93c71168.

--- ab_utils/__init__.py
+++ ab_utils/__init__.py
@@ -2,5 +2,5 @@ import matplotlib.pyplot as plt
 
 from . import log
 from . import vis
-from .method_chains import flatten_columns, assign_with_apply, assign_from_split, read, write, add_global
+from .method_chains import flatten_columns, assign_with_apply, read, write
 from .smk_utils import inject_snakemake
