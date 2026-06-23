c0a8836564c32fa9364eb21c43f41fd159b60482
Alex <alex.blakes@gmail.com>
2026-06-23 11:26:29 +0100

    Add timestamp to log file path

--- ab_utils/smk.py
+++ ab_utils/smk.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
@@ -175,5 +176,6 @@ def log_path(script: str, snakefile: str) -> str:
     """
     snakefile_dir = Path(snakefile).parent.relative_to(Path.cwd())
     dot_path = ".".join(snakefile_dir.parts)
+    timestamp = datetime.now().isoformat(timespec="milliseconds")
     script_stem = Path(script).stem
-    return f"log/{dot_path}.{script_stem}.log"
+    return f"log/{dot_path}.{timestamp}.{script_stem}.log"
